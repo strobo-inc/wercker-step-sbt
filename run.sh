8623f82181028a4b0513fb922e8f9632c09aa968
Kawajiri Ryoma <ryoma.edison@gmail.com>
2016-06-03 15:58:46 +0900

    Enable cache

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ ! -n "$WERCKER_SBT_TASK" ]; then
     error "Please specify an $WERCKER_SBT_TASK"
 fi
 
-sbt $WERCKER_SBT_TASK
+sbt -sbt-dir $WERCKER_CACHE_DIR/.sbt -sbt-boot $WERCKER_CACHE_DIR/.sbt/boot -ivy $WERCKER_CACHE_DIR/.ivy  $WERCKER_SBT_TASK
